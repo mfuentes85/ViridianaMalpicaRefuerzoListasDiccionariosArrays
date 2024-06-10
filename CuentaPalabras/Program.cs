53c0f71bdc01d0adb528f96763e6dd2bdabbfa7c
mfuentes85 <mfuentes85@gmail.com>
2024-06-09 22:38:22 -0600

    CuentaPalabras

---
+++ CuentaPalabras/Program.cs
@@ -0,0 +1,29 @@
+﻿string Texto;
+string[] palabras = new string[] { };
+
+
+Console.WriteLine("Por favor introduzca una frase o un texto");
+Texto = Console.ReadLine();
+
+palabras = Texto.Split(' ');
+
+Dictionary<string, int> ContPal = new Dictionary<string, int>();
+
+foreach (string palabra in palabras)
+{
+    if (ContPal.ContainsKey(palabra))
+    {
+        ContPal[palabra]++;
+    }
+    else
+    {
+        ContPal[palabra] = 1;
+    }
+ }
+
+
+foreach (var conteo in ContPal)
+{
+    Console.WriteLine($"{ conteo.Key} : { conteo.Value}"); 
+}
+   
